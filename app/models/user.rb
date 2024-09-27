ef60bcf40bdb0aa9189852b81cc900194108a8e3
sakura <ichigomilk35412@gmail.com>
2024-09-27 23:29:32 +0900

    メール機能修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,4 @@ class User < ApplicationRecord
   has_secure_password
   has_one_attached :profile_image # Active Storageの設定
   validates :password, length: { minimum: 6 }
- 
-  # ユーザー作成時にメールを送信
-  after_create_commit :send_welcome_email
-
-  private
-
-  def send_welcome_email
-    UserMailer.with(user: self).welcome_email.deliver_later
-  end
-
 end
